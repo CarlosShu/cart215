ebc05f2c002c81797074041bedc3361121718b7a
CarlosShu <70170956+CarlosShu@users.noreply.github.com>
2020-11-24 09:18:47 -0500

    P4: Added a looooooot of stuff.

--- Project_04/js/Cow.js
+++ Project_04/js/Cow.js
@@ -4,6 +4,7 @@ class Cow {
     this.y = y;
     this.xSize = xSize;
     this.ySize = ySize;
+    this.randommoo = 0.01;
   }
 
   display() {
@@ -11,5 +12,33 @@ class Cow {
     imageMode(CENTER);
     image(cowidleimage, this.x, this.y, this.xSize, this.ySize);
     pop();
+
+    // 3 second timer.
+    if (mootimeleft <= 3 && mootimeleft > 0) {
+      if (mootimecounter == 30) {
+        mootimecounter = 0;
+        mootimeleft--;
+      }
+      mootimecounter++;
+    }
+
+    let r = random(0, 1);
+    if (r < this.randommoo) {
+      mootimeleft = 3;
+    }
+
+    if (mootimeleft <= 3 && mootimeleft > 0) {
+      // User score.
+      push();
+      textAlign(CENTER, CENTER);
+      textFont(gamefont);
+      textSize(10);
+      fill(255, 255, 255);
+      text("Moo", this.x - 7, this.y - 40);
+      pop();
+    }
+    if (mootimeleft == 0) {
+      mootimeleft = 0;
+    }
   }
 }
--- Project_04/js/UFO.js
+++ Project_04/js/UFO.js
@@ -1,31 +1,23 @@
 class UFO {
-  constructor(x, y, xSize, ySize) {
+  constructor(x, y, xSize, ySize, speed) {
     this.x = x;
     this.y = y;
     this.xSize = xSize;
     this.ySize = ySize;
     this.vx = 0;
     this.vy = 0;
-    this.speed = 10;
-    this.jitteriness = 0.1; // How likely the ufo is to change direction
-    this.distance = undefined;
+    this.speed = speed;
+    this.vx = this.speed;
   }
 
   move() {
-    // First check if we should change direction
-    let r = random(0, 1);
-    if (r < this.jitteriness) {
-      this.vx = random(-this.speed, this.speed);
-      this.vy = random(-this.speed, this.speed);
+    this.x += this.vx;
+  }
+
+  bounce() {
+    if (this.x < 0 || this.x > width) {
+      this.vx = -this.vx;
     }
-
-    // Update position with velocity to actually move
-    this.x = this.x + this.vx;
-    this.y = this.y + this.vy;
-
-    // Constrain to the canvas (guess it's a walled garden!)
-    this.x = constrain(this.x, 0, width);
-    this.y = constrain(this.y, 100, 300);
   }
 
   display() {
